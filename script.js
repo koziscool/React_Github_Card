b7719832e45a0c0f1500150497d3165a91fa4d6d
koziscool <kozjob@yahoo.com>
2016-10-06 12:53:13 -0500

    card w harcode data

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
+var Card = React.createClass({
+
+  getInitialState: function() {
+    return null;
+  },
+
+  render: function() {
+    return (
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/7587245?v=3" width="80" />
+        <h3>Kit</h3>
+        <hr/>
+      </div>
+    );
+  }
+
+});
+
+
 var Main = React.createClass({
   
   render: function() {
     return (
       <div>
-        tetris
+        <Card/>
       </div>
     )
   }
